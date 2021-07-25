f78dc0008dbc7cbc4be2cf365b44df3f603042c0
unknown <thaivanphu.qn95@gmail.com>
2021-07-25 22:33:14 +0700

    Populdate DB

---
+++ populate/FakeDB.js
@@ -0,0 +1,19 @@
+const { portfolios } = require("./data");
+const Portfolio = require("../db/models/portfolio");
+
+class FakeDB {
+  async clean() {
+    await Portfolio.deleteMany({});
+  }
+
+  async addData() {
+    await Portfolio.create(portfolios);
+  }
+
+  async populate() {
+    await this.clean();
+    await this.addData();
+  }
+}
+
+module.exports = new FakeDB();
