982df6f5e0f0bd0e208bc074da50bdbcc966bb51
unknown <thaivanphu.qn95@gmail.com>
2021-07-25 22:21:56 +0700

    Create portfolio data

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -1,3 +1,7 @@
-exports.getPortfolios = (req, res) => {
-  return res.json({ data: [10, 20, 30, 40] });
+const mongoose = require("mongoose");
+const Portfolio = mongoose.model("Portfolio");
+
+exports.getPortfolios = async (req, res) => {
+  const portfolios = await Portfolio.find({});
+  return res.json(portfolios);
 };
--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const config = require("../config/dev");
 const mongoose = require("mongoose");
 
+require("./models/portfolio");
+
 exports.connect = () => {
   return mongoose.connect(
     config.DB_URI,
