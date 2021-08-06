32e368f989cfebc5882edfcbfa6a83c9148bac39
unknown <thaivanphu.qn95@gmail.com>
2021-08-06 23:42:50 +0700

    update some code

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// Authentication middleware
+// This middleware will check access token in authorization headers
+// of a request
+// It will verify access token against Auth0 JSON web key set
+exports.checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10,
+    jwksUri: "https://vanphu-qn95.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https://vanphu-qn95.us.auth0.com/api/v2/",
+  issuer: "https://vanphu-qn95.us.auth0.com/",
+  algorithms: ["RS256"],
+});
