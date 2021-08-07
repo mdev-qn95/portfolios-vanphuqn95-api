5ace168da12272a93f1266a3ec695f1a7408e2da
unknown <thaivanphu.qn95@gmail.com>
2021-08-07 10:40:09 +0700

    Blog endpoint

--- populate/data.js
+++ populate/data.js
@@ -34,6 +34,32 @@ const data = {
       endDate: "01/01/2011",
     },
   ],
+  blogs: [
+    {
+      slug: "my-first-blog",
+      title: "My First Blog",
+      subTitle: "It was chilly winter day...",
+      content: "<p>Some very nice content</p>",
+      userId: "google-oauth2|105472611650941991142",
+      status: "published",
+    },
+    {
+      slug: "my-second-blog",
+      title: "My Second Blog",
+      subTitle: "It was hot summer day...",
+      content: "<p>Some very nice content</p>",
+      userId: "google-oauth2|105472611650941991142",
+      status: "published",
+    },
+    {
+      slug: "my-third-blog",
+      title: "My Third Blog",
+      subTitle: "It was rainy spring day...",
+      content: "<p>Some very nice content</p>",
+      userId: "google-oauth2|105472611650941991142",
+      status: "published",
+    },
+  ],
 };
 
 module.exports = data;
--- routes/blogs.js
+++ routes/blogs.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { checkJwt, checkRole } = require("../controllers/auth");
-const { getBlogs } = require("../controllers/blogs");
+const {
+  getBlogs,
+  getBlogById,
+  getBlogBySlug,
+} = require("../controllers/blogs");
 
 router.get("", getBlogs);
+router.get("/:id", getBlogById);
+router.get("/s/:slug", getBlogBySlug);
 
 module.exports = router;
