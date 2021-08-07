c997b873410f3a94517f9cd6d2f502ee1e86dccc
unknown <thaivanphu.qn95@gmail.com>
2021-08-07 22:59:50 +0700

    Add GET endpoint to test

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ async function runServer() {
   server.use("/api/v1/portfolios", require("./routes/portfolios"));
   server.use("/api/v1/blogs", require("./routes/blogs"));
 
+  server.get("/test", (req, res) => {
+    res.json({ message: "Hello" });
+  });
+
   const PORT = parseInt(process.env.PORT, 10) || 3001;
   server.listen(PORT, (err) => {
     if (err) console.error(err);
