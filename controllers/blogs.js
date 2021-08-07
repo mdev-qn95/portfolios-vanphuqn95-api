f2a15fa58d9b2cd22cfcf5d1eb1a2a5a25ec168a
unknown <thaivanphu.qn95@gmail.com>
2021-08-07 10:27:48 +0700

    Blog server

---
+++ controllers/blogs.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const Blog = mongoose.model("Blog");
+
+exports.getBlogs = (req, res) => {
+  return res.json({ message: "Its working!" });
+};
---
+++ routes/blogs.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const { checkJwt, checkRole } = require("../controllers/auth");
+const { getBlogs } = require("../controllers/blogs");
+
+router.get("", getBlogs);
+
+module.exports = router;
